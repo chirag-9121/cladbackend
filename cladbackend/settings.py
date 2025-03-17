da6143b767be3a8348e29c40403b6047a032f7cc
chirag-9121 <chirag9121.cg@gmail.com>
2025-03-17 21:15:53 +0530

    vercel configuration

--- cladbackend/settings.py
+++ cladbackend/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = os.getenv('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
-ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost' , '.vercel.app']
 
 
 # Application definition
@@ -74,7 +74,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'cladbackend.wsgi.application'
+WSGI_APPLICATION = 'cladbackend.wsgi.app'
 
 
 # Database
@@ -136,7 +136,7 @@ CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    # "https://frontend-domain.com",
+    os.getenv('FRONTEND_URL')
 ]
 
 # CORS_ALLOW_METHODS = [
